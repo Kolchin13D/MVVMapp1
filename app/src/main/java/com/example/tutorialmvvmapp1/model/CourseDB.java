0872cd26483d8fcb164cebde9d69884f61c19ae8
Kolchin13D <kolchin13d@gmail.com>
2024-05-31 16:05:02 +0300

    add DAO to course and category

---
+++ app/src/main/java/com/example/tutorialmvvmapp1/model/CourseDB.java
@@ -0,0 +1,43 @@
+package com.example.tutorialmvvmapp1.model;
+
+import android.content.Context;
+
+import androidx.annotation.NonNull;
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+import androidx.sqlite.db.SupportSQLiteDatabase;
+
+@Database(entities = {Category.class, Course.class}, version = 1)
+public abstract class CourseDB extends RoomDatabase {
+
+    public abstract CategoryDAO categoryDAO();
+    public abstract CourseDAO courseDAO();
+
+    private static CourseDB instance;
+
+    public static synchronized CourseDB getInstance(Context context){
+
+        if(instance == null){
+            instance = Room.databaseBuilder(context.getApplicationContext(this),
+                    CourseDB.class, "courses_DB")
+                    .fallbackToDestructiveMigration()
+                    .addCallback(roomCallback)
+                    .build();
+        }
+        return instance;
+    }
+
+    // callback
+    private RoomDatabase.Callback roomCallback = new RoomDatabase.Callback(){
+        @Override
+        public void onCreate(@NonNull SupportSQLiteDatabase db) {
+            super.onCreate(db);
+
+            //  insert data when DB create
+
+
+        }
+    };
+
+}
